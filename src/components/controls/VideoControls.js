c75d849b7b9f045244b61b135a3ff1a9c49e3b6a
Weitong Huang <wxh170430@gmail.com>
2026-06-07 14:18:59 -0700

    Add dynamic center control visibility

--- src/components/controls/VideoControls.js
+++ src/components/controls/VideoControls.js
@@ -1,4 +1,5 @@
 import "./VideoControls.css";
+import { onConnected } from "@utils/observer";
 import ScreenShareButton from "./ScreenShareButton";
 import ParamSelector from "./ParamSelector";
 import VolumeButton from "./VolumeButton";
@@ -6,13 +7,34 @@ import PipButton from "./PipButton";
 import FullscreenButton from "./FullscreenButton";
 
 function VideoControls() {
+  let centerControl = '';
+
+  const isNotMobile = !window.matchMedia('(max-width: 768px)').matches;
+
+  const connectedCallback = () => {
+    const centerControl = document.getElementById('center-control');
+
+    const showCenterControl = () => {
+        centerControl.classList.remove('hidden');
+    };
+
+    const hideCenterControl = () => {
+      centerControl.classList.add('hidden');
+    };
+
+    window.addEventListener('addremotevideo', hideCenterControl);
+    window.addEventListener('removeremotevideo', showCenterControl);
+  }
+
+  if (isNotMobile) {
+    centerControl = `<div class="center-control" id="center-control"> ${ScreenShareButton()} ${ParamSelector()} </div>`;
+    onConnected('center-control', connectedCallback);
+  }
+
   return (`
     <div id="video-controls">
       <div id="bottom-controls">
-        <div class="center-control">
-          ${ScreenShareButton()}
-          ${ParamSelector()}
-        </div>
+        ${centerControl}
   
         <div class="right-control">
           ${VolumeButton()}
--- src/components/video/VideoPlaceholder.js
+++ src/components/video/VideoPlaceholder.js
@@ -23,7 +23,7 @@ function VideoPlaceholder() {
 
   return (`
     <div id="video-placeholder">
-      点击下方开始你的共享
+      请等待共享开始 ...
     </div>
   `);
 }
